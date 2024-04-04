ae2983a321d568478cb61f5dbc55d6db8b20c8ed
PrayasPathak <prayasroman1@gmail.com>
2024-04-04 19:26:04 +0545

    Related fields and foreign key serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,20 +1,37 @@
 from rest_framework import serializers
 from .models import Product
+from api.serializers import UserPublicSerializer
+
+
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail", lookup_field="pk", read_only=True
+    )
+    title = serializers.CharField(read_only=True)
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    user = UserPublicSerializer(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail", lookup_field="pk", read_only=True
+    )
+    related_products = ProductInlineSerializer(
+        source="user.product_set.all", many=True, read_only=True
+    )
 
     class Meta:
         model = Product
         fields = [
-            # "user",
+            "user",
+            "url",
             "id",
             "title",
             "content",
             "price",
             "sale_price",
             "my_discount",
+            "related_products",
         ]
 
     def get_my_discount(self, obj):
